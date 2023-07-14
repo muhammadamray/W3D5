88318c37ec283a7f0cbc0cf6e359655185b32483
Sean <pca.sean.jeun@gmail.com>
2023-07-14 14:57:17 -0400

    ::parent=/::add_child/::remove_child method built

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,4 @@
+require "byebug"
 class PolyTreeNode
 
     attr_reader :parent, :children, :value
@@ -7,5 +8,25 @@ class PolyTreeNode
         @children = []
     end
 
-    
+    def parent=(node)
+        if self.parent != nil 
+            self.parent.children.delete(self)
+        end
+        @parent = node # first parent
+        
+        if @parent != nil
+            node.children << self if !node.children.include?(self)
+        end
+
+    end 
+
+    def add_child(child)
+        child.parent = self
+    end
+
+    def remove_child(child)
+        child.parent = nil
+        raise "error!" if !child.value.include?("child")
+    end
+
 end
