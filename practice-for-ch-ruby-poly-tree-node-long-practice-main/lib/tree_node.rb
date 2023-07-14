ed2afb9d32d7c26587b53ba50563088ceb6b0ab7
Sean <pca.sean.jeun@gmail.com>
2023-07-14 12:10:39 -0400

    ::initialize method built

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,11 @@
 class PolyTreeNode
-  
+
+    attr_reader :parent, :children, :value
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    
 end
